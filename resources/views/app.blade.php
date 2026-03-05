82cc774c42ca4ab1d928652118b3b83ea5e91455
Maiev <antdor24@gmail.com>
2026-03-06 00:50:37 +0200

    add google analytics tags

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,17 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=SN+Pro:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet">
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-D5JGFXNY7D"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-D5JGFXNY7D');
+    </script>
+
     @viteReactRefresh
     @routes
 
